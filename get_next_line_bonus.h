46f9256b8439d081cb5dc3b2f848c4f8ac4bc3b5
Nicolas Metais <nmetais@made-f0Ar8s6.clusters.42paris.fr>
2024-11-28 20:48:51 +0100

    git gud

---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmetais <nmetais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 16:24:00 by nmetais           #+#    #+#             */
+/*   Updated: 2024/11/28 16:25:31 by nmetais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*ft_strjoin(char *s1, char*s2);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmetais <nmetais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 23:52:47 by nmetais           #+#    #+#             */
+/*   Updated: 2024/11/28 16:25:45 by nmetais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	i = start;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		len = 0;
+	else if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	while (j < len)
+	{
+		str[j] = s[i];
+		j++;
+		i++;
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int				i;
+	unsigned char	d;
+
+	if (!s || !c)
+		return (NULL);
+	i = 0;
+	d = (unsigned char) c;
+	if (d == '\0')
+		return ((char *)s + ft_strlen(s));
+	while (s[i])
+	{
+		if (s[i] == d)
+			return ((char *) s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin( char *s1, char*s2)
+{
+	int		i;
+	size_t	j;
+	char	*str;
+	int		size1;
+
+	if (s1 == NULL)
+	{
+		s1 = malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
+	str = malloc(sizeof(char) * ((int)ft_strlen(s1) + (int)ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	size1 = ft_strlen(s1);
+	i = -1;
+	while (i++, i < size1)
+		str[i] = s1[i];
+	j = 0;
+	while (i < size1 + (int)ft_strlen(s2))
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}
